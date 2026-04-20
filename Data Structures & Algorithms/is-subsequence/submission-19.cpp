9a9934cb116223aa49af59f7786f6ab3a8b2fa5a
Rubykid07 <134687330+Rubykid07@users.noreply.github.com>
2026-04-20 22:27:36 +0700

    Add: is-subsequence - submission-19

---
+++ Data Structures & Algorithms/is-subsequence/submission-19.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j = 0;
+        int n = (int)t.size();
+        for(int i = 0; i < n; i++){
+            if(t[i] == s[j]){
+                j++;
+            }
+        }
+        return j == (int)s.length() ;
+    }
+};
